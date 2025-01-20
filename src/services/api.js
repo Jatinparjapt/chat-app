fd43d496e31117dffebf9a3fa7df8aa07d918583
jatinkumar <jatin3961jk@gmail.com>
2025-01-20 20:08:34 +0530

    chat

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000"; // Replace with your backend URL
+const SOCKET_URL = "https://chat-app-backend-uw58.onrender.com"; // Replace with your backend URL
 export const socket = io(SOCKET_URL);
